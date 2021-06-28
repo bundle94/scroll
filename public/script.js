639c63a9fe5910bb692b9b03ac27c762b271b1aa
bundle94 <akobundumichael94@gmail.com>
2021-06-28 03:48:29 +0100

    Changed app package name

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  //host: 'scrollpeerjs.herokuapp.com',
-  //port: '443'
-  host: '/',
-  port: '3001'
+  host: 'scrollpeerjs.herokuapp.com',
+  port: '443'
+  //host: '/',
+  //port: '3001'
 })
 let myVideoStream;
 let currentPeer;
