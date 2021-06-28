f58c8d14caeb8882cff91434e64e71cee2bd7249
bundle94 <akobundumichael94@gmail.com>
2021-06-28 03:42:11 +0100

    Added features like share screen, mute and disable video

--- public/script.js
+++ public/script.js
@@ -1,9 +1,13 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'scrollpeerjs.herokuapp.com',
-  port: '443'
+  //host: 'scrollpeerjs.herokuapp.com',
+  //port: '443'
+  host: '/',
+  port: '3001'
 })
+let myVideoStream;
+let currentPeer;
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
@@ -11,34 +15,41 @@ navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  myVideoStream = stream;
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
+      currentPeer = call.peerConnection;
       addVideoStream(video, userVideoStream)
     })
   })
 
   socket.on('user-connected', userId => {
-    console.log(userId);
+    alertify.success(userId + ' has joined the conversation');
     connectToNewUser(userId, stream)
   })
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  if (peers[userId]) { 
+    peers[userId].close();
+    alertify.error(userId + ' left the conversation');
+  }
 })
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
 })
 
+
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
+    currentPeer = call.peerConnection;
     addVideoStream(video, userVideoStream)
   })
   call.on('close', () => {
@@ -54,4 +65,94 @@ function addVideoStream(video, stream) {
     video.play()
   })
   videoGrid.append(video)
+}
+
+const endCall = () => {
+  alert("Currently working on this feature");
+}
+
+document.getElementById("shareScreen").addEventListener('click', (e) => {
+  navigator.mediaDevices.getDisplayMedia({
+    video: {
+      cursor: "always"
+    },
+    audio: {
+      echoCancellation: true,
+      noiseSuppression: true
+    }
+  }).then(stream => {
+    let videoTrack = stream.getVideoTracks()[0];
+    videoTrack.onended = function() {
+      stopScreenShare();
+    }
+    let sender = currentPeer.getSenders().find(function(s) {
+      return s.track.kind == videoTrack.kind;
+    })
+    sender.replaceTrack(videoTrack);
+  }).catch(error => {
+    console.log("Unable to get display media "+ error);
+    alertify.error("Unable to get display media");
+  })
+})
+
+function stopScreenShare() {
+  let videoTrack = myVideoStream.getVideoTracks()[0];
+  let sender = currentPeer.getSenders().find(function(s) {
+    return s.track.kind == videoTrack.kind;
+  })
+  sender.replaceTrack(videoTrack);
+}
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
 }
